a735d704178c9b51e216628c4da74b590881e690
0p3r4t0r <robert83sutherland@gmail.com>
2021-06-27 22:09:27 +0900

    fitness: install, add models with admin.

--- fitness/admin.py
+++ fitness/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Exercise, Set, Workout
 
-# Register your models here.
+
+@admin.register(Exercise)
+class ExercseAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Set)
+class Set(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Workout)
+class Workout(admin.ModelAdmin):
+    pass
--- fitness/models.py
+++ fitness/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Exercise(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+
+class Set(models.Model):
+    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
+    reps = models.IntegerField()
+    seconds_on = models.IntegerField(help_text="Time working out.")
+    seconds_off = models.IntegerField(help_text="Time resting between sets.") 
+
+
+class Workout(models.Model):
+    sets = models.ManyToManyField(Set, verbose_name="list of sets", through='Routine')
+
+
+class Routine(models.Model):
+    ordinal = models.IntegerField()
+    set = models.ForeignKey(Set, on_delete=models.CASCADE)
+    workout = models.ForeignKey(Workout, on_delete=models.CASCADE)
+
